5fe4d3f56cef333c294fbc90f99b151dbd04f59b
Patrick <patrickyambrick@gmail.com>
2024-03-20 19:32:57 -0400

    Add Dropdown

---
+++ components/Dropdown.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Dropdown.module.css';
+
+export function Dropdown({ submenu }: any) {
+	return (
+		<ul className={`${styles.dropdown}`}>
+			<li>hi</li>
+		</ul>
+	);
+}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,27 @@
 import styles from '../styles/components/Nav.module.css';
+import navLinks from '../data/nav.json';
+import { Dropdown } from './Dropdown';
 export function Nav() {
-	return <nav className={`${styles.nav}`}>Home</nav>;
+	const renderNavLink = (link: any) => {
+		if (link.urls) {
+			return (
+				<select>
+					{link.urls.map((submenu: any, index: number) => {
+						return <Dropdown submenu={submenu} key={index} />;
+					})}
+				</select>
+			);
+		} else if (link.url) {
+			return <a href={link.url}>{link.text}</a>;
+		}
+	};
+	return (
+		<nav className={`${styles.nav}`}>
+			<ul>
+				{navLinks.map((link, index) => {
+					return <li key={index}>{renderNavLink(link)}</li>;
+				})}
+			</ul>
+		</nav>
+	);
 }
