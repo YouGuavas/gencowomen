cd5863b34857a007549e347ff4c5d8fd17ae5d0e
Patrick <patrickyambrick@gmail.com>
2024-03-21 16:04:02 -0400

    Remove dropdown

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,25 +1,16 @@
 import styles from '../styles/components/Nav.module.css';
 import navLinks from '../data/nav.json';
-import { Dropdown } from './Dropdown';
 export function Nav() {
-	const renderNavLink = (link: any) => {
-		if (link.urls) {
-			return (
-				<>
-					{link.urls.map((submenu: any, index: number) => {
-						return <Dropdown submenu={submenu} key={index} />;
-					})}
-				</>
-			);
-		} else if (link.url) {
-			return <a href={link.url}>{link.text}</a>;
-		}
-	};
 	return (
 		<nav className={`${styles.nav}`}>
 			<ul>
 				{navLinks.map((link, index) => {
-					return <li key={index}>{renderNavLink(link)}</li>;
+					return (
+						<li key={index}>
+							{' '}
+							<a href={link.url}>{link.text}</a>
+						</li>
+					);
 				})}
 			</ul>
 		</nav>
