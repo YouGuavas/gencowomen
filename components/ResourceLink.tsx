6f821c04f286d169016f41a0a62ae3618aac82e1
Patrick <patrickyambrick@gmail.com>
2024-03-23 12:43:35 -0400

    Start styling Link component

--- components/ResourceLink.tsx
+++ components/ResourceLink.tsx
@@ -1,10 +1,13 @@
 import { LinkType } from '@/types/types';
 import Link from 'next/link';
+import styles from '../styles/components/ResourceLink.module.css';
 
 const ResourceLink: React.FC<LinkType> = ({ title, url }) => {
 	return (
 		<>
-			<Link href={url}>{title}</Link>
+			<Link className={`${styles.link}`} href={url}>
+				{title}
+			</Link>
 		</>
 	);
 };
--- components/Table.tsx
+++ components/Table.tsx
@@ -2,11 +2,11 @@ import ResourceLink from './ResourceLink';
 import { LinkType, TableProps } from '@/types/types';
 const Table: React.FC<TableProps> = ({ links }) => {
 	return (
-		<>
+		<ul className="p-12 grid grid-cols-3 gap-2">
 			{links.map((link: LinkType, index) => {
 				return <ResourceLink title={link.title} url={link.url} />;
 			})}
-		</>
+		</ul>
 	);
 };
 module.exports = { Table };
