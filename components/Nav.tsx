d86bde70082eb6f7232526dac0e27076c91310f4
Patrick <patrickyambrick@gmail.com>
2024-04-09 19:11:30 -0400

    Responsive nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -15,7 +15,11 @@ export function Nav() {
 		<nav
 			className={`background-2 rounded-md md:p-1 flex flex-col md:flex-row gap-1 md:gap-2 text-xs md:text-base`}
 		>
-			<button onClick={toggleMenu} className="md:hidden p-2 focus:outline-none">
+			<button
+				onClick={toggleMenu}
+				className="md:hidden p-2 w-10 content-center justify-center text-center transition duration-700 ease-in-out"
+				aria-label="Toggle Menu"
+			>
 				<svg
 					className="w-6 h-6"
 					fill="none"
@@ -40,9 +44,15 @@ export function Nav() {
 					)}
 				</svg>
 			</button>
-			<div className={`md:flex ${isMenuOpen ? 'block' : 'hidden'}`}>
+			<div
+				className={`flex flex-col md:flex-row ${
+					isMenuOpen || window.innerWidth > 767 ? 'block' : 'hidden'
+				} transition-max-height duration-700 ease-in-out overflow-hidden`}
+				style={{ maxHeight: '500px' }}
+			>
 				{navLinks.map((link, index) => (
 					<Link
+						onClick={toggleMenu}
 						className="px-1 py-2 md:px-2 md:py-4 color-7 hover-color-6 background-2 hover-background-1 duration-700 rounded-md uppercase font-medium tracking-wider content-center"
 						key={index}
 						href={link.url}
